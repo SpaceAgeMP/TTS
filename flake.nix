45ec3d0875e56fb6adff6797b6e3048415af09ae
Mark Dietzer <git@doridian.net>
2025-11-17 21:36:53 -0800

    Update package import method in flake.nix (#407)

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
         package = pkgs.buildGoModule {
           pname = "spaceage-tts";
           version = "0.1.0";
